e69e35cfb62d2e74da846f2859617802badaf0f3
Rene <rene@sdo.sh>
2022-06-13 20:29:05 +0200

    feature: added a test

--- test/tokenizer_test.dart
+++ test/tokenizer_test.dart
@@ -1,12 +1,10 @@
 import 'dart:async';
-
 import 'package:test/test.dart';
 import 'package:tokenizer/token.dart';
-
 import 'package:tokenizer/tokenizer.dart';
 
 void main() {
-  test('tokenizes a string', () async {
+  test('tokenizes a string, space as delimiter', () async {
     final string = 'Lorem ipsum dolor';
     final tokenizer = Tokenizer();
     final c = StreamController<String>();
@@ -24,6 +22,29 @@ void main() {
         Token(' '),
         Token('ipsum'),
         Token(' '),
+        Token('dolor')
+      ]),
+    );
+  });
+  test('tokenizes a string, space and comma as delimiter', () async {
+    final string = 'Lorem, ipsum-dolor';
+    final tokenizer = Tokenizer({' ', ',', '-'});
+    final c = StreamController<String>();
+
+    c.add(string);
+    c.close();
+
+    final tokens =
+        await c.stream.transform(tokenizer.streamTransformer).toList();
+
+    expect(
+      tokens,
+      equals([
+        Token('Lorem'),
+        Token(','),
+        Token(' '),
+        Token('ipsum'),
+        Token('-'),
         Token('dolor')
       ]),
     );
